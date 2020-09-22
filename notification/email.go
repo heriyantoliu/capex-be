8de7629c01854fac615d0814c6c0ff09493a7a65
Heriyanto <heriyanto.liu@gmail.com>
2020-09-22 08:36:40 +0700

    fix email bug (noreply)

--- notification/email.go
+++ notification/email.go
@@ -18,7 +18,7 @@ func SendEmail(to []string, subject string, templateHTML string, bodyVar interfa
 
 	var mailServerUsername string = os.Getenv("SMTPUser")
 	var mailServerPassword string = os.Getenv("SMTPPassword")
-	const sender string = "noreply@sidomuncul.co.id"
+	const sender string = "fotocopy@sidomuncul.co.id"
 
 	const headers string = "MIME-version: 1.0;\nContent-Type: text/html;"
 
@@ -31,7 +31,7 @@ func SendEmail(to []string, subject string, templateHTML string, bodyVar interfa
 
 	var body bytes.Buffer
 
-	gmailAuth := smtp.PlainAuth("", mailServerUsername, mailServerPassword, "smtp-relay.gmail.com")
+	gmailAuth := smtp.PlainAuth("", mailServerUsername, mailServerPassword, "smtp.gmail.com")
 
 	body.Write([]byte(fmt.Sprintf("From: %s\nSubject: %s\n%s\n\n", sender, subject, headers)))
 
@@ -39,7 +39,7 @@ func SendEmail(to []string, subject string, templateHTML string, bodyVar interfa
 	if err != nil {
 		log.Println(err.Error())
 	}
-	err = smtp.SendMail("smtp-relay.gmail.com:587", gmailAuth, sender, to, body.Bytes())
+	err = smtp.SendMail("smtp.gmail.com:587", gmailAuth, sender, to, body.Bytes())
 	if err != nil {
 		log.Println(err.Error())
 	}
